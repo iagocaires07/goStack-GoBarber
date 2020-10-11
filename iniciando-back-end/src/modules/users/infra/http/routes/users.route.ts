14f4223f4d2c2095a4764449ebd2493c1135b6db
Iago Caires <cairesiago07@gmail.com>
2020-10-11 09:46:14 -0300

    Redis integrado a aplicação. 🛠

--- iniciando-back-end/src/modules/users/infra/http/routes/users.route.ts
+++ iniciando-back-end/src/modules/users/infra/http/routes/users.route.ts
@@ -11,7 +11,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 const usersRoute = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRoute.post(
   '/',
