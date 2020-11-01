ae6cbd32fb844a3b4854acadedf779524056340f
Iago Caires <cairesiago07@gmail.com>
2020-10-31 22:05:24 -0300

    Continuando a implemantação das  telas.
    Feita a lisatagem dos providers em tela e iniciada a implementeção da tela de criação de appointments.

--- appgobarber/src/pages/Dashboard/index.tsx
+++ appgobarber/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
-import Button from '../../components/Button';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -11,15 +12,44 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProviderList,
+  ProviderContainer,
+  PoviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderText,
+  ProvidersListTitle,
 } from './styles';
 
+export interface Providers {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Providers[]>([]);
+
   const { singOut, user } = useAuth();
   const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
   }, [navigate]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate],
+  );
 
   return (
     <Container>
@@ -33,7 +63,35 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
-      <Button onPress={singOut}>Sair</Button>
+
+      <ProviderList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <PoviderAvatar source={{ uri: provider.avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderText>Segunda à sexta</ProviderText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderText>8h às 18h</ProviderText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
     </Container>
   );
 };
--- iniciando-back-end/src/services/UpdateUserAvatarSevice.ts
+++ iniciando-back-end/src/services/UpdateUserAvatarSevice.ts
@@ -1,4 +1,4 @@
-import { getRepository } from 'typeorm';
+import {getRepository} from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 import uploadConfig from '../config/upload';
@@ -7,26 +7,26 @@ import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
-interface Request {
+interface Request{
   user_id: string;
   avatarFilname: string;
 }
 
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatarFilname }: Request): Promise<User> {
+  public async execute({user_id, avatarFilname}: Request): Promise<User>{
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne(user_id);
 
-    if (!user) {
+    if(!user){
       throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
-    if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.tmpFolder, user.avatar);
+    if(user.avatar){
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
       const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-      if (userAvatarFileExists) {
+      if(userAvatarFileExists){
         await fs.promises.unlink(userAvatarFilePath);
       }
     }
@@ -36,6 +36,7 @@ class UpdateUserAvatarService {
     await usersRepository.save(user);
 
     return user;
+
   }
 }
 
